e542e83c695eccb34773485dd4abf7c8daf59d8e
Yingjun Cui <yingjun@buildrr.com>
2013-12-21 14:19:22 -0600

    add allTest.php file

---
+++ Test/Case/allTest.php
@@ -0,0 +1,26 @@
+<?php
+class AllTests extends PHPUnit_Framework_TestSuite {
+
+/**
+ * Suite define the tests for this suite
+ *
+ * @return void
+ */
+	public static function suite() {
+		$suite = new PHPUnit_Framework_TestSuite('All Tests');
+
+		$path = APP_TEST_CASES . DS;
+		// core tests
+		$pluginsPath = APP . 'Plugin' . DS;
+		$testPath = DS . 'Test' . DS . 'Case';
+		$modelPath = $testPath . DS . 'Model';
+		$controllerPath = $testPath . DS . 'Controller';
+
+		$behaviorPath = $modelPath . DS . 'Behavior';
+
+		$suite->addTestFile($testPath  . DS . 'AuctionTest.php');
+
+		return $suite;
+	}
+
+}
